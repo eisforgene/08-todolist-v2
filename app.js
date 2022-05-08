a485e38299cadc233dea2e7d8adee810cd143764
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-05-08 00:55:40 -0700

    able to delete from list

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
@@ -37,41 +37,56 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully added to database.");
-  }
+app.get("/", function (req, res) {
+
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully added to database.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      console.log(foundItems);
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  })
+});
+
+app.post("/", function (req, res) {
+
+  const itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+  res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, (err => { // requires a callback function to execute
+    if (!err) { // if no errors
+      console.log("Successfully deleted checked item.")
+      res.redirect("/");
+    }
+  }))
 })
 
-app.get("/", function(req, res) {
-
-  res.render("list", {listTitle: "Today", newListItems: items});
-
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
-
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
-
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("Server started on port " + PORT);
 });
